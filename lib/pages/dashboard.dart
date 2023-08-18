67af748a547a95e646eab8796fa25d82c36bea0e
Michael Wekesa <wekesa360@outlook.com>
2023-08-18 07:14:43 +0300

    update: add dashboard UI, fetch and login post

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,21 +1,112 @@
+// ignore_for_file: use_build_context_synchronously
+import 'dart:convert';
+import 'package:cpims_flutter_test_app/widgets/snackBarWidget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 
-
+import '../tokenProvider.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
 
   @override
-  State<Dashboard> createState() => _DashboardState();
+  DashboardState createState() => DashboardState();
 }
 
+class DashboardState extends State<Dashboard> {
+  Map<String, dynamic> dashboardData = {};
 
-class _DashboardState extends State<Dashboard> {
+  Future<void> fetchData() async {
+    final String url =
+        dotenv.env["API_BASE_URL"]! + dotenv.env["LOGIN_ENDPOINT"]!;
+
+    final String? token =
+        Provider.of<TokenProvider>(context, listen: false).token;
+    final Map<String, String> headers = {
+      "Authorization": "Bearer $token",
+    };
+
+    final response = await http.get(url as Uri, headers: headers);
+
+    if (response.statusCode == 200) {
+      setState(() {
+        dashboardData = json.decode(response.body);
+      });
+    } else {
+      snackBarWidget(context, "Error fetching data: ${response.statusCode}");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('API Dashboard'),
+      ),
       body: Center(
-        child: Text('Login Page'),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              for (var entry in dashboardData.entries)
+                DashboardItem(
+                  title: entry.key,
+                  value: entry.value,
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DashboardItem extends StatelessWidget {
+  final String title;
+  final dynamic value;
+
+  const DashboardItem({super.key, 
+    required this.title,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        // You can add onTap functionality here if needed
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 12.0),
+        decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.grey[300]!)),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 6),
+            Text(
+              value.toString(),
+              style: const TextStyle(fontSize: 14),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,90 +1,137 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import '../tokenProvider.dart';
+import '../widgets/snackBarWidget.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
   @override
-  State<Login> createState() => _LoginState();
+  State<Login> createState() => LoginState();
 }
 
-
-class _LoginState extends State<Login> {
-
+class LoginState extends State<Login> {
   final TextEditingController _usernameController = TextEditingController();
   final FocusNode _focusNodePassword = FocusNode();
   final TextEditingController _passwordController = TextEditingController();
   bool _obscurePassword = true;
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Form(
-        child: Column(
-          children: [
-            const SizedBox(height: 10),
-              Text(
-                "Login to your account",
-                style: Theme.of(context).textTheme.bodyMedium,
+          child: Column(
+        children: [
+          const SizedBox(height: 10),
+          Text(
+            "Login to your account",
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          const SizedBox(height: 60),
+          TextFormField(
+            controller: _usernameController,
+            keyboardType: TextInputType.name,
+            decoration: InputDecoration(
+              labelText: "Email",
+              prefixIcon: const Icon(Icons.email_outlined),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(
+                10,
+              )),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
               ),
-              const SizedBox(height: 60),
-              TextFormField(
-                controller: _usernameController,
-                keyboardType: TextInputType.name,
-                decoration: InputDecoration(
-                  labelText: "Email",
-                  prefixIcon: const Icon(Icons.email_outlined),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10,)
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                onEditingComplete: () => _focusNodePassword.requestFocus(),
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return "Please enter email.";
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                controller: _passwordController,
-                focusNode: _focusNodePassword,
-                obscureText: _obscurePassword,
-                keyboardType: TextInputType.visiblePassword,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  prefixIcon: const Icon(Icons.password_outlined),
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        _obscurePassword = !_obscurePassword;
-                      });
-                    },
-                    icon: _obscurePassword
+            ),
+            onEditingComplete: () => _focusNodePassword.requestFocus(),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return "Please enter email.";
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            controller: _passwordController,
+            focusNode: _focusNodePassword,
+            obscureText: _obscurePassword,
+            keyboardType: TextInputType.visiblePassword,
+            decoration: InputDecoration(
+              labelText: "Password",
+              prefixIcon: const Icon(Icons.password_outlined),
+              suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _obscurePassword = !_obscurePassword;
+                    });
+                  },
+                  icon: _obscurePassword
                       ? const Icon(Icons.visibility_outlined)
-                        : const Icon(Icons.visibility_off_outlined)
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return "Please enter password.";
-                  }
-                  return null;
-                },
+                      : const Icon(Icons.visibility_off_outlined)),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
               ),
-              const SizedBox(height: 60),
-          ],
-        )
-      ),
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return "Please enter password.";
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 60),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size.fromHeight(50),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            onPressed: _login,
+            child: const Text("Login"),
+          ),
+        ],
+      )),
     );
+  }
+
+  Future<void> _login() async {
+    final String url =
+        dotenv.env["API_BASE_URL"]! + dotenv.env["LOGIN_ENDPOINT"]!;
+
+    final Map<String, dynamic> body = {
+      "username":  _usernameController.text,
+      "password": _passwordController.text,
+    };
+
+    final Map<String, String> headers = {
+      "Content-Type": "application/json",
+    };
+
+    final response = await http.post(url as Uri, body: body, headers: headers);
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      final String token = data["token"];
+      final TokenProvider tokenProvider =
+          Provider.of<TokenProvider>(context, listen: false);
+      tokenProvider.setToken(token);
+      Navigator.pushReplacementNamed(context, "/dashboard");
+    } else if (response.statusCode == 401) {
+      snackBarWidget(context, "Invalid username or password.");
+    } else {
+      snackBarWidget(context, "Login failed.");
+    }
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    _focusNodePassword.dispose();
+    super.dispose();
   }
 }
