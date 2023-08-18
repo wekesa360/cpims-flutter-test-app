6dca5bc1e5d2a5965b3806f605dc1bc562f2ece5
Michael Wekesa <wekesa360@outlook.com>
2023-08-18 06:24:19 +0300

    update: login UI

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -11,11 +11,79 @@ class Login extends StatefulWidget {
 
 
 class _LoginState extends State<Login> {
+
+  final TextEditingController _usernameController = TextEditingController();
+  final FocusNode _focusNodePassword = FocusNode();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _obscurePassword = true;
+
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Page'),
+    return Scaffold(
+      body: Form(
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+              Text(
+                "Login to your account",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              const SizedBox(height: 60),
+              TextFormField(
+                controller: _usernameController,
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  prefixIcon: const Icon(Icons.email_outlined),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10,)
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onEditingComplete: () => _focusNodePassword.requestFocus(),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter email.";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: _passwordController,
+                focusNode: _focusNodePassword,
+                obscureText: _obscurePassword,
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  prefixIcon: const Icon(Icons.password_outlined),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _obscurePassword = !_obscurePassword;
+                      });
+                    },
+                    icon: _obscurePassword
+                      ? const Icon(Icons.visibility_outlined)
+                        : const Icon(Icons.visibility_off_outlined)
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter password.";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 60),
+          ],
+        )
       ),
     );
   }
